2f8b42e346f42ec7cf618f4a21c4548e8ee9b1f4
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-04-14 09:49:09 -0400

    adding tables to load

---
+++ src/pg/test/sql/02_load_tables_test.sql
@@ -0,0 +1,8 @@
+\i test/fixtures/obs_table.sql
+\i test/fixtures/obs_column.sql
+\i test/fixtures/obs_column_table.sql
+\i test/fixtures/obs_column_to_column.sql
+\i test/fixtures/obs_85328201013baa14e8e8a4a57a01e6f6fbc5f9b1.sql
+\i test/fixtures/obs_3e7cc9cfd403b912c57b42d5f9195af9ce2f3cdb.sql
+
+set search_path to cdb_observatory,public;

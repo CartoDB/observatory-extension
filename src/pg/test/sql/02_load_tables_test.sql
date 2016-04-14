eeded3d4204aa68cfd4d80068a28c3380ba51e57
Rafa de la Torre <rtorre@cartodb.com>
2016-04-14 15:53:40 +0200

    Load the rest of the fixtures

--- src/pg/test/sql/02_load_tables_test.sql
+++ src/pg/test/sql/02_load_tables_test.sql
@@ -1,6 +1,9 @@
-SET client_min_messages TO NOTICE;
+SET client_min_messages TO WARNING;
 \set ECHO none
-\echo Loading obs_table.sql fixture file...
+\echo Loading fixtures...
 \i test/fixtures/obs_table.sql
+\i test/fixtures/obs_column_table.sql
+\i test/fixtures/obs_column.sql
+\i test/fixtures/obs_column_to_column.sql
 \echo Done.
 \unset ECHO

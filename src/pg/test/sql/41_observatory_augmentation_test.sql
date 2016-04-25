d686b6b4362b3bb1775142e36ec960cb5d727369
John Krauss <irving.krauss@gmail.com>
2016-04-25 18:20:26 -0400

    fix namespace issue

--- src/pg/test/sql/41_observatory_augmentation_test.sql
+++ src/pg/test/sql/41_observatory_augmentation_test.sql
@@ -1,7 +1,7 @@
 \i test/sql/load_fixtures.sql
 --
 WITH result as(
-  Select count(OBS_GetDemographicSnapshot->>'value') expected_columns
+  Select count(cdb_observatory.OBS_GetDemographicSnapshot->>'value') expected_columns
   FROM cdb_observatory.OBS_GetDemographicSnapshot(cdb_observatory._TestPoint())
 ) select expected_columns ='58' as OBS_GetDemographicSnapshot_test_no_returns
 FROM result;

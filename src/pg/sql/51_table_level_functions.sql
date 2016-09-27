56fd9346c00e069a2267e44a8ebeb3f31ec869ec
John Krauss <irving.krauss@gmail.com>
2016-09-27 21:34:24 +0000

    logic behind area interpolation for all aggregation types

--- src/pg/sql/51_table_level_functions.sql
+++ src/pg/sql/51_table_level_functions.sql
@@ -4,14 +4,12 @@
 --
 --
 
---select cdb_observatory._obs_getmeasureresultmetadata('{"numer_ids":["us.census.acs.B03002006", "us.census.acs.B03002012"], "denom_ids":["us.census.acs.B01003001", "us.census.acs.B01003001"], "geom_ids": ["us.census.tiger.census_tract", "us.census.tiger.state"], "timespans": ["2006 - 2010", "2010 - 2014"], "geom": "0101000020E610000000000000807A54C0C3CF8DC4FADB4240" }'::json);
-
 CREATE OR REPLACE FUNCTION cdb_observatory._OBS_GetMeasureResultMetadata(params json)
 RETURNS cdb_observatory.ds_return_metadata
 AS $$
 DECLARE
-  colnames text[]; -- Array to store the name of the measures to be returned
-  coltypes text[]; -- Array to store the type of the measures to be returned
+  colnames text[];
+  coltypes text[];
   requested_measures text[];
   measure_id text;
 BEGIN
@@ -40,8 +38,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
---select cdb_observatory._obs_getmeasurequery('public', 'testinput', '{"numer_ids":["us.census.acs.B03002006", "us.census.acs.B03002012"], "denom_ids":["us.census.acs.B01003001", "us.census.acs.B01003001"], "geom_ids": ["us.census.tiger.census_tract", "us.census.tiger.state"], "timespans": ["2006 - 2010", "2010 - 2014"], "geom": "0101000020E610000000000000807A54C0C3CF8DC4FADB4240" }'::json);
-
 
 CREATE OR REPLACE FUNCTION cdb_observatory._OBS_GetMeasureQuery(
   table_schema text, table_name text, params json)
@@ -62,9 +58,6 @@ DECLARE
   geom_table_name text;
   data_table_name text;
 BEGIN
-
-    -- Deconstruct JSON args
-
     EXECUTE
       $query$
         WITH _filters AS (SELECT
@@ -73,23 +66,81 @@ BEGIN
           unnest($3) geom_id,
           unnest($4) timespan
         )
-        SELECT
-           String_Agg(numer_tablename || '.' || numer_colname ||
-                      '/NullIf(' || denom_tablename || '.' || denom_colname || ', 0)' || numer_colname, ', ') AS colspecs,
-           (SELECT String_Agg(tablename, ', ') FROM (SELECT JSONB_Object_Keys(JSONB_Object(
-              Array_Cat(Array_Agg('observatory.' || numer_tablename),
-                Array_Cat(Array_Agg('observatory.' || geom_tablename),
-                          Array_Agg('observatory.' || denom_tablename))),
-              Array_Cat(Array_Agg(numer_tablename),
-                Array_Cat(Array_Agg(geom_tablename),
-                          Array_Agg(denom_tablename)))
-            )) tablename) bar) tablenames,
-           String_Agg(numer_tablename || '.' || numer_geomref_colname || ' = ' || denom_tablename || '.' || denom_geomref_colname ||
-            ' AND ' || denom_tablename || '.' || denom_geomref_colname || ' = ' || geom_tablename || '.' || geom_geomref_colname,
-            ' AND ') AS obs_wheres,
-           String_Agg('ST_Intersects(' || geom_tablename || '.' ||  geom_colname
-              || ', ' || $5 || '.' || $6 || '.the_geom)', ' AND ')
-              AS user_wheres
+        SELECT String_Agg(CASE
+          -- denominated
+          WHEN denom_id IS NOT NULL THEN ' CASE ' ||
+            -- denominated point-in-poly or user polygon is same as OBS polygon
+            ' WHEN ST_GeometryType(FIRST(_user_table.the_geom)) = ''ST_Point'' ' ||
+            '      OR FIRST(_user_table.the_geom = ' || geom_tablename || '.' || geom_colname || ')' ||
+            ' THEN FIRST(' || numer_tablename || '.' || numer_colname ||
+            '      / NullIf(' || denom_tablename || '.' || denom_colname || ', 0))' ||
+            -- denominated polygon interpolation
+            -- SUM ((numer / denom) * (% user geom in OBS geom))
+            ' ELSE ' ||
+            --' NULL END '
+            ' SUM((' || numer_tablename || '.' || numer_colname || '/NullIf(' || denom_tablename || '.' || denom_colname || ', 0)) ' ||
+            ' * CASE WHEN ST_Within(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ') THEN 1 ' ||
+            '        WHEN ST_Within(' || geom_tablename || '.' || geom_colname || ', _user_table.the_geom) THEN ' ||
+            '          ST_Area(' || geom_tablename || '.' || geom_colname || ') ' ||
+            '          / ST_Area(_user_table.the_geom)' ||
+            '        ELSE (ST_Area(ST_Intersection(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ')) ' ||
+            '         / ST_Area(_user_table.the_geom))' ||
+            '   END) END '
+          -- areaNormalized
+          WHEN numer_aggregate ILIKE 'sum' THEN ' CASE ' ||
+            -- areaNormalized point-in-poly or user polygon is the same as OBS polygon
+            ' WHEN ST_GeometryType(FIRST(_user_table.the_geom)) = ''ST_Point'' ' ||
+            '      OR FIRST(_user_table.the_geom = ' || geom_tablename || '.' || geom_colname || ')' ||
+            ' THEN FIRST(' || numer_tablename || '.' || numer_colname ||
+            '      / (ST_Area(' || geom_tablename || '.' || geom_colname || '::Geography)/1000000)) ' ||
+            -- areaNormalized polygon interpolation
+            -- SUM (numer * (% OBS geom in user geom)) / area of big geom
+            ' ELSE ' ||
+            --' NULL END '
+            ' SUM(' || numer_tablename || '.' || numer_colname || ' ' ||
+            ' * CASE WHEN ST_Within(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ') ' ||
+            '         THEN ST_Area(_user_table.the_geom) / ST_Area(' || geom_tablename || '.' || geom_colname || ') ' ||
+            '        WHEN ST_Within(' || geom_tablename || '.' || geom_colname || ', _user_table.the_geom) ' ||
+            '         THEN 1 ' ||
+            '        ELSE (ST_Area(ST_Intersection(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ')) ' ||
+            '         / ST_Area(' || geom_tablename || '.' || geom_colname || '))' ||
+            '   END) END '
+          -- prenormalized
+          ELSE ' CASE ' ||
+            -- predenominated point-in-poly or user polygon is the same as OBS- polygon
+            ' WHEN ST_GeometryType(FIRST(_user_table.the_geom)) = ''ST_Point'' ' ||
+            '      OR FIRST(_user_table.the_geom = ' || geom_tablename || '.' || geom_colname || ')' ||
+            ' THEN FIRST(' || numer_tablename || '.' || numer_colname || ') ' ||
+            ' ELSE ' ||
+            -- predenominated polygon interpolation
+            -- TODO should weight by universe instead of area
+            -- SUM (numer * (% user geom in OBS geom))
+            ' SUM((' || numer_tablename || '.' || numer_colname || ') ' ||
+            ' * CASE WHEN ST_Within(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ') THEN 1 ' ||
+            '        WHEN ST_Within(' || geom_tablename || '.' || geom_colname || ', _user_table.the_geom) THEN ' ||
+            '          ST_Area(' || geom_tablename || '.' || geom_colname || ') ' ||
+            '          / ST_Area(_user_table.the_geom)' ||
+            '        ELSE (ST_Area(ST_Intersection(_user_table.the_geom, ' || geom_tablename || '.' || geom_colname || ')) ' ||
+            '         / ST_Area(_user_table.the_geom))' ||
+            '   END) END '
+          END || ' ' || numer_colname, ', ') AS colspecs,
+
+          (SELECT String_Agg(tablename, ', ') FROM (SELECT JSONB_Object_Keys(JSONB_Object(
+             Array_Cat(Array_Agg('observatory.' || numer_tablename),
+               Array_Cat(Array_Agg('observatory.' || geom_tablename),
+                         Array_Agg('observatory.' || denom_tablename) FILTER (WHERE denom_tablename IS NOT NULL))),
+             Array_Cat(Array_Agg(numer_tablename),
+               Array_Cat(Array_Agg(geom_tablename),
+                         Array_Agg(denom_tablename) FILTER (WHERE denom_tablename IS NOT NULL)))
+           )) tablename) bar) tablenames,
+          String_Agg(numer_tablename || '.' || numer_geomref_colname || ' = ' ||
+                     geom_tablename || '.' || geom_geomref_colname ||
+           Coalesce(' AND ' || numer_tablename || '.' || numer_geomref_colname || ' = ' ||
+                               denom_tablename || '.' || denom_geomref_colname, ''),
+           ' AND ') AS obs_wheres,
+          String_Agg('ST_Intersects(' || geom_tablename || '.' ||  geom_colname
+             || ', _user_table.the_geom)', ' AND ')
+             AS user_wheres
         FROM observatory.obs_meta
         WHERE (numer_id, coalesce(denom_id, ''), geom_id, numer_timespan)
            IN (SELECT numer_id, denom_id, geom_id, timespan FROM _filters)
@@ -99,34 +150,15 @@ BEGIN
     USING (SELECT ARRAY(SELECT json_array_elements_text(params->'numer_ids'))::text[]),
           (SELECT ARRAY(SELECT json_array_elements_text(params->'denom_ids'))::text[]),
           (SELECT ARRAY(SELECT json_array_elements_text(params->'geom_ids'))::text[]),
-          (SELECT ARRAY(SELECT json_array_elements_text(params->'timespans'))::text[]),
-          table_schema, table_name;
+          (SELECT ARRAY(SELECT json_array_elements_text(params->'timespans'))::text[]);
 
     data_query := format($query$
-      SELECT %s, %s.%s.cartodb_id::int FROM %s, %s.%s WHERE %s AND %s
-    $query$, colspecs, table_schema, table_name, tables, table_schema, table_name, obs_wheres, user_wheres);
+      SELECT %s, _user_table.cartodb_id::int
+      FROM %s, %s.%s _user_table
+      WHERE %s
+        AND %s
+      GROUP BY _user_table.cartodb_id
+    $query$, colspecs, tables, table_schema, table_name, obs_wheres, user_wheres);
     RETURN data_query;
-
-    --FOREACH measure_id IN ARRAY measure_ids_arr
-    --LOOP
-    --  -- Build query to compute each value and normalize
-    --  -- Assumes the default normalization method, the normalize parameter given in the JSON
-    --  -- should be checked in order to build the final query
-    --  SELECT measures_query || ' sum(' || measure_id || '/fraction)::numeric as ' || measure_id || ', ' INTO measures_query;
-    --END LOOP;
-
-    -- Data query should select the measures and the cartodb_id of the user table, in that order.
-    --data_query := '(WITH _areas AS(SELECT ST_Area(a.the_geom::geography)'
-    --    || '/ (1000 * 1000) as fraction, a.geoid, b.cartodb_id FROM '
-    --    || geom_table_name || ' as a, '
-    --    || table_schema || '.' || table_name || ' AS b '
-    --    || 'WHERE b.the_geom && a.the_geom ), values AS (SELECT geoid, '
-    --    || measures_list
-    --    || ' FROM ' || data_table_name || ' ) '
-    --    || 'SELECT '
-    --    || measures_query
-    --    || ' cartodb_id::int FROM _areas, values '
-    --    || 'WHERE values.geoid = _areas.geoid GROUP BY cartodb_id);';
-    --RETURN data_query;
 END;
 $$ LANGUAGE plpgsql;
